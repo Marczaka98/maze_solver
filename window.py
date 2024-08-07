30e33bc2e7e4f21ef04b6091d1a6290bf302db53
Marczaka98 <austin.marczak98@gmail.com>
2024-08-07 16:43:10 -0400

    Added Cell class to draw cells on the display window. Plus cleaned up some stuff in the other classes

--- window.py
+++ window.py
@@ -3,9 +3,9 @@ from tkinter import Tk, BOTH, Canvas
 class Window:
     def __init__(self, width, height):
         self.root = Tk()
-        self.title = ''
-        self.canvas = Canvas(self.root,height=height, width=width)
-        self.canvas.pack()
+        self.root.title("Maze Solver")
+        self.canvas = Canvas(self.root, bg="white", height=height, width=width)
+        self.canvas.pack(fill=BOTH, expand=1)
         self.running = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -15,13 +15,13 @@ class Window:
 
     def wait_for_close(self):
         self.running = True
-        while self.running == True:
+        while self.running:
             self.redraw()
     
     def close(self):
         self.running = False
 
-    def draw_line(self, line, fill_color):
+    def draw_line(self, line, fill_color="black"):
         line.draw(self.canvas, fill_color)
 
 class Point:
@@ -34,7 +34,7 @@ class Line:
         self.point1 = p1
         self.point2 = p2
 
-    def draw(self, canvas, fill_color):
+    def draw(self, canvas, fill_color="black"):
         canvas.create_line(
             self.point1.x,
             self.point1.y,
@@ -44,3 +44,33 @@ class Line:
             width = 2 
         )
     
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(x1,y1),Point(x1,y2))
+            self._win.draw_line(line,"black")
+        if self.has_top_wall:
+            line = Line(Point(x1,y1),Point(x2,y1))
+            self._win.draw_line(line,"black")
+        if self.has_right_wall:
+            line = Line(Point(x2,y1),Point(x2,y2))
+            self._win.draw_line(line,"black")
+        if self.has_bottom_wall:
+            line = Line(Point(x1,y2),Point(x2,y2))
+            self._win.draw_line(line,"black")
+        
