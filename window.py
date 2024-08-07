c44d693ffd4ea796f0904f4ea9ce3618bde1bcf1
Marczaka98 <austin.marczak98@gmail.com>
2024-08-07 17:01:17 -0400

    Added draw_move function to draw a line from the center of one cell to another

--- window.py
+++ window.py
@@ -73,4 +73,12 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1,y2),Point(x2,y2))
             self._win.draw_line(line,"black")
+
+    def draw_move(self, to_cell, undo=False):
+        starting_point = Point((self._x1 + self._x2)//2,(self._y1 + self._y2)//2)
+        ending_point = Point((to_cell._x1 + to_cell._x2)//2,(to_cell._y1 + to_cell._y2)//2)
+        if undo:
+            self._win.draw_line(Line(starting_point,ending_point),"red")
+        else:
+            self._win.draw_line(Line(starting_point,ending_point),"gray")
         
