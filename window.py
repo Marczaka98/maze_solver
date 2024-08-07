4573cd1f252c492d09ac305584c4dcb1afe77ac4
Marczaka98 <austin.marczak98@gmail.com>
2024-08-07 17:43:17 -0400

    Did some function call cleanup and added cases for not declaring a window to tests

--- window.py
+++ window.py
@@ -45,7 +45,7 @@ class Line:
         )
     
 class Cell:
-    def __init__(self, win):
+    def __init__(self, win=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
@@ -57,22 +57,24 @@ class Cell:
         self._win = win
 
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
         self._x1 = x1
         self._y1 = y1
         self._x2 = x2
         self._y2 = y2
         if self.has_left_wall:
             line = Line(Point(x1,y1),Point(x1,y2))
-            self._win.draw_line(line,"black")
+            self._win.draw_line(line)
         if self.has_top_wall:
             line = Line(Point(x1,y1),Point(x2,y1))
-            self._win.draw_line(line,"black")
+            self._win.draw_line(line)
         if self.has_right_wall:
             line = Line(Point(x2,y1),Point(x2,y2))
-            self._win.draw_line(line,"black")
+            self._win.draw_line(line)
         if self.has_bottom_wall:
             line = Line(Point(x1,y2),Point(x2,y2))
-            self._win.draw_line(line,"black")
+            self._win.draw_line(line)
 
     def draw_move(self, to_cell, undo=False):
         starting_point = Point((self._x1 + self._x2)//2,(self._y1 + self._y2)//2)
