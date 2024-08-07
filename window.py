445ca3806c5d9d7e3e03b37522c198cd2b4eae9a
Marczaka98 <austin.marczak98@gmail.com>
2024-08-07 14:45:09 -0400

    Added two new classes (Point & Line) that allow lines to be drawn on the display window

--- window.py
+++ window.py
@@ -20,3 +20,27 @@ class Window:
     
     def close(self):
         self.running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.point1 = p1
+        self.point2 = p2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.point1.x,
+            self.point1.y,
+            self.point2.x,
+            self.point2.y,
+            fill = fill_color,
+            width = 2 
+        )
+    
